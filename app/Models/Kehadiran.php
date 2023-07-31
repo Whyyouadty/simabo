3bbae463f4b6d0342c98ab593df9017d1ebeb935
whyyouadty <wahyugit987@gmail.com>
2023-07-31 13:32:21 +0800

    tebel kehadiran

--- app/Models/Kehadiran.php
+++ app/Models/Kehadiran.php
@@ -8,11 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Kehadiran extends Model
 {
     use HasFactory;
-    use HasFactory;
     protected $table = 'kehadiran';
     protected $fillable = [
         'id',
-        'user_id',
+        'pegawai_id',
         'tanggal',
         'jam_masuk',
         'jam_keluar',
@@ -23,32 +22,32 @@ class Kehadiran extends Model
         'updated_at'
     ];
 
-    public function user()
+    public function pegawai()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(Pegawai::class, 'pegawai_id');   
     }
 
     public function gate()
     {
-        return $this->belongsTo(Gate::class, 'gate_id');
+        return $this->belongsTo(Gate::class, 'gate_id');   
     }
-
+    
     public function scopejoinList($query)
     {
         return $query
-            ->leftJoin('user as model_a', 'kehadiran.user_id', '=', 'model_a.id')
-            ->leftJoin('gate as model_b', 'kehadiran.gate_id', '=', 'model_b.id')
-            ->select(
-                'kehadiran.id',
-                'model_a.nama',
-                'kehadiran.tanggal',
-                'kehadiran.jam_masuk',
-                'kehadiran.jam_keluar',
-                'kehadiran.status',
-                'kehadiran.keterangan',
-                'model_b.no_sesi as sesi',
-                'user.created_at',
-                'user.updated_at',
-            );
+        ->leftJoin('pegawai as model_a', 'kehadiran.pegawai_id', '=', 'model_a.id')
+        ->leftJoin('gate as model_b', 'kehadiran.gate_id', '=', 'model_b.id')    
+        ->select(
+            'kehadiran.id',
+            'model_a.nama',
+            'kehadiran.tanggal',
+            'kehadiran.jam_masuk',
+            'kehadiran.jam_keluar',
+            'kehadiran.status',
+            'kehadiran.keterangan',
+            'model_b.no_sesi as sesi',
+            'kehadiran.created_at',
+            'kehadiran.updated_at'
+        );     
     }
 }
--- resources/views/pages/kehadiran.blade.php
+++ resources/views/pages/kehadiran.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.Dashboard')
+@extends('layout.Base')
 @section('content')
 <div class="col-xl-12">
     <div class="card">
@@ -36,7 +36,7 @@
                         @foreach ($data['kehadiran'] as $item)
                         <tr>
                             <td style="width: 10%">{{$no++}}</td>
-                            <td style="width: 10%">{{ $item->user->nama }}</td>
+                            <td style="width: 10%">{{ $item->pegawai->nama }}</td>
                             <td style="width: 10%">{{ $item->tanggal }}</td>
                             <td style="width: 10%">{{ $item->jam_masuk }}</td>
                             <td style="width: 10%">{{ $item->jam_keluar }}</td>
@@ -78,9 +78,9 @@
                         <input type="hidden" name="id" id="dataId">
                         <div class="col-12 col-md-6">
                             <label class="form-label">User</label><br>
-                            <select name="user_id" id="user_id" class="form-select" required>
+                            <select name="pegawai_id" id="pegawai_id" class="form-select" required>
                                 <option value="" selected disabled>--pilih--</option>
-                                @foreach ($data['user'] as $d)
+                                @foreach ($data['pegawai'] as $d)
                                     <option value="{{$d->id}}">{{$d->nama}}</option>
                                 @endforeach
                             </select>
@@ -106,7 +106,7 @@
                             <select name="status" id="status" class="form-select" required>
                                 <option value="" selected disabled>--pilih--</option>
                                 <option value="Masuk">Masuk</option>
-                                <option value="Tidak masuk">Tidak masuk</option>
+                                <option value="Tidak Masuk">Tidak Masuk</option>
                             </select>
                             <span class="text-danger error-msg small" id="nama-alert"></span>
                         </div>
@@ -169,7 +169,7 @@
                 $('#btn-simpan'    ).val  ("edit-user"          );
                 $('#nama-alert'    ).html ('                   ');
                 $('#modal-data'    ).modal('show'               );
-                $('#user_id'       ).val  (res.data.user_id);
+                $('#pegawai_id'       ).val  (res.data.pegawai_id);
                 $('#tanggal'         ).val  (res.data.tanggal);
                 $('#jam_masuk'  ).val  (res.data.jam_masuk);
                 $('#jam_keluar'       ).val  (res.data.jam_keluar);
